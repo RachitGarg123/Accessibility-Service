cb8d4a6282a533fe557bbc8c30d5da80a3b33955
rachit <rachitgarg386@gmail.com>
2024-04-10 16:03:58 +0530

    Added Comments for better understanding

--- app/src/main/java/com/example/accessibilityservicepresentation/MyAccessibilityService.kt
+++ app/src/main/java/com/example/accessibilityservicepresentation/MyAccessibilityService.kt
@@ -10,6 +10,8 @@ import com.example.accessibilityservicepresentation.AppConstants.DEMO_APP
 
 class MyAccessibilityService: AccessibilityService() {
 
+    // this is optional and called once initially our accessibility service starts
+    // Use this Method to perform one time setup
     override fun onServiceConnected() {
         val info = AccessibilityServiceInfo()
         info.apply {
@@ -23,14 +25,24 @@ class MyAccessibilityService: AccessibilityService() {
         this.serviceInfo = info
     }
 
+/*     this is the required method and this function is called everytime when any of the
+       mentioned Accessibility event occurs and we can retrieve the window content using
+       event.source which gives us an object of Accessibility Node Info through we can
+       traverse the entire tree using recursion */
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
 
     }
 
+/*     this is the required method
+     the system calls this method when the system wants to interrupt the feedback
+     your service is providing, usually in response to a user action such as
+     moving focus to a different control */
     override fun onInterrupt() {
 
     }
 
+    // this is optional method and will be called when the service is ending or unbinding itself
+    // use this method to perform work you need to before the service stops
     override fun onUnbind(intent: Intent?): Boolean {
         return super.onUnbind(intent)
     }
